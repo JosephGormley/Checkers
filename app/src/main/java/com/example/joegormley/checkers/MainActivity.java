9fe03ca934037e0eec44bc57fa49491e712d07a5
JosephGormley <josephbgormley@gmail.com>
2017-12-20 18:50:34 -0500

    Main Menu - XML
    Buttons result with opening respective Activity
    Menu is now landscape/portrait friendly

--- app/src/main/java/com/example/joegormley/checkers/MainActivity.java
+++ app/src/main/java/com/example/joegormley/checkers/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.joegormley.checkers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openGame(View view){
+        Intent i = new Intent(this, GameActivity.class);
+        startActivity(i);
+    }
+
+    public void openSettings(View view){
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
     }
 }
